a76bb4470e8fff2a3071ceb3905df4800f89519b
diwakarrana <diwakarrana464@gmail.com>
2020-02-26 14:34:43 +0530

    Singleton class is created

---
+++ coreJavaPrograms/src/single_ton_class/SingleTonClass.java
@@ -0,0 +1,31 @@
+package single_ton_class;
+
+public class SingleTonClass {
+	
+	int check;
+	
+	static SingleTonClass obj = null;
+	static int count ;
+	
+	 private SingleTonClass()
+	{
+		 
+	}
+	 
+	 static Object getInstance()
+	 {
+		 if(count==0)
+			{
+				obj = new SingleTonClass();
+				count++;
+			}
+		return obj;
+	 }
+	public static void main(String[] args) {
+		
+		SingleTonClass x = (SingleTonClass) SingleTonClass.getInstance();
+		SingleTonClass y = (SingleTonClass) SingleTonClass.getInstance();
+		x.check = 10;
+		System.out.println(y.check);
+	}
+}
